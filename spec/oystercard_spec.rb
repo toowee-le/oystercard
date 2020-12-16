92f421fcb70938889e66e7a56974b3abeb0c6abb
Tuyet Le <toowee.le@gmail.com>
2020-12-16 11:04:02 +0000

    Refactor spec file with nested describe for easier code readability

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,44 +5,46 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
-  it 'allow the user to top up balance' do
-    subject.top_up(10)
-    expect(subject.balance).to eq 10
+  describe '#top_up' do
+    it 'allow the user to top up balance' do
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
+  
+    it 'has maximum balance limit of £90' do
+      subject.top_up(Oystercard::BALANCE_LIMIT)
+      expect {subject.top_up(1)}.to raise_error "Limit exceeded #{Oystercard::BALANCE_LIMIT}"
+    end
   end
 
-  it 'has maximum limit of £90' do
-    subject.top_up(Oystercard::BALANCE_LIMIT)
-    expect {subject.top_up(1)}.to raise_error "Limit exceeded #{Oystercard::BALANCE_LIMIT}"
+  describe '#in_journey' do
+    it 'in_journey should start as false' do
+      expect(subject.in_journey).to eq false
+    end
   end
 
-  # it 'deduct fare from balance' do
-  #   subject.top_up(Oystercard::BALANCE_LIMIT)
-  #   subject.deduct(10)
-  #   expect(subject.balance).to eq Oystercard::BALANCE_LIMIT - 10
-  # end
+  describe '#touch_in' do
+    it 'has minimum balance amount of £1' do
+      expect { subject.touch_in }.to raise_error "Please top up"
+    end
 
-  it 'in_journey? should start as false' do
-    expect(subject.in_journey).to eq false
+    it 'touch_in sets the journey to true' do
+      subject.top_up(10)
+      subject.touch_in
+      expect(subject.in_journey).to eq true
+    end
   end
+  
+  describe '#touch_out' do
+    it 'touch_out sets the journey to false' do
+      subject.touch_out
+      expect(subject.in_journey).to eq false
+    end
 
-  it 'touch_in sets the journey to true' do
-    subject.top_up(10)
-    subject.touch_in
-    expect(subject.in_journey).to eq true
-  end
-
-  it 'touch_out sets the journey to false' do
-    subject.touch_out
-    expect(subject.in_journey).to eq false
-  end
-
-  it 'has minimum amount of £1' do
-    expect { subject.touch_in }.to raise_error "Please top up"
-  end
-
-  it '#touch_out reduces the balance by minimum fare' do
-    subject.top_up(30)
-    subject.touch_in
-    expect{ subject.touch_out }.to change{ subject.balance }.by -Oystercard::MINIMUM_CHARGE
+    it 'touch_out reduces the balance by minimum charge' do
+      subject.top_up(30)
+      subject.touch_in
+      expect{ subject.touch_out }.to change{ subject.balance }.by -Oystercard::MINIMUM_CHARGE
+    end
   end
 end
